4a9300ce5a6df13c40c00b121667d77855cc576e
zwelisha <zwescientist@gmail.com>
2024-04-30 14:34:04 +0200

    Refactored hero section, reversed columns

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -4,29 +4,14 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import DottedBox from '@/components/dotted-box';
-import OrangeBox from '@/components/orange-box'
+import OrangeBox from '@/components/orange-box';
 import ButtonLink from '@/components/button';
+
 const Hero = () => {
     return (
         <Container className="mt-5 mb-5"> 
             <Row>
-                <Col lg={5}>
-                    <div className="mb-5">
-                        <h4 className="text-orange text-uppercase mt-5">Work with us</h4>
-                        <h3 className="text-grey">Power up with our electrical services!</h3>
-                        <p className="text-grey">
-                            The services rendered by Siavista can cover a project in its entirety from original concept to acceptance of completed installation work
-                        </p>
-                        <ButtonLink 
-                            className="orange-btn"
-                            backgroundColor="#f9921d" 
-                            textColor="#ffffff"
-                            text="explore more"
-                            url="/services"
-                        />
-                    </div>
-                </Col>
-                <Col lg={7}>
+                <Col lg={7} order={2} className="order-lg-1"> 
                     <div id="hero-image-container">
                         <div id="dotted-box">
                             <DottedBox/>
@@ -46,8 +31,25 @@ const Hero = () => {
                         </div>
                     </div>
                 </Col>
+                <Col lg={5} order={1} className="order-lg-2"> 
+                    <div className="mb-5">
+                        <h4 className="text-orange text-uppercase mt-5">Work with us</h4>
+                        <h3 className="text-grey">Power up with our unmatched service!</h3>
+                        <p className="text-grey">
+                            The services rendered by Siavista can cover a project in its entirety from original concept to acceptance of completed installation work
+                        </p>
+                        <ButtonLink 
+                            className="orange-btn"
+                            backgroundColor="#f9921d" 
+                            textColor="#ffffff"
+                            text="explore more"
+                            url="/services"
+                        />
+                    </div>
+                </Col>
             </Row>
         </Container>
     );
 }
+
 export default Hero;
