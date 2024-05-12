9404f2843b7a9cdc2cc3f5889226bebaf03513c7
zwelisha <zwescientist@gmail.com>
2024-05-12 14:26:51 +0200

    Added explore more button that leads to services page on all service cards

--- src/components/services/index.js
+++ src/components/services/index.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
-import { Container, Row, Col, Card } from 'react-bootstrap';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+import ButtonLink from '@/components/button';
 
 const Services = ({ services, columnSize }) => {
   return (
@@ -10,11 +11,17 @@ const Services = ({ services, columnSize }) => {
           <Col md={columnSize} key={index} className="mb-5">
             <Card className="text-center service-card">
               <Card.Img className="service-card-image" variant="top" src={service.imageUrl} />
-              <Card.Body>
+              <Card.Body className="mb-5">
                 <Card.Title className="text-orange">{service.title}</Card.Title>
-                <Card.Text className="text-grey">
+                <Card.Text className="text-grey service-card-description">
                   {service.description}
                 </Card.Text>
+                <ButtonLink
+                  backgroundColor="#f9921d"
+                  textColor="#ffffff"
+                  text="explore more"
+                  url="/services"
+                />
               </Card.Body>
             </Card>
           </Col>
