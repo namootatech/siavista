2dd0d6cb5dc41b6bcb3fb7adc3d2be594ec845fe
Ayabonga Qwabi <aya@simply.co.za>
2024-05-24 16:39:01 +0200

    add google analytics and hotjar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,7 +4,10 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { Poppins } from 'next/font/google';
 import NextNProgress from 'nextjs-progressbar';
-
+import { hotjar } from 'react-hotjar';
+import { useEffect } from 'react';
+import * as gtag from '@/lib/gtag';
+import { useRouter } from 'next/router';
 // If you are looking for the css theme files, they live in the public folder
 // to add your own css simply extend the main.css file in the styles folder
 
@@ -16,8 +19,36 @@ const poppins = Poppins({
 });
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    hotjar.initialize(4997210, 6);
+  }, []);
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <div className='main'>
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      ></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');`,
+        }}
+      ></script>
       <div id='main-container' className={`${poppins.variable}`}>
         <NextNProgress color='#f9921d' />
         <Component {...pageProps} />
