834f84d807eab96f20fbf7f1769afd8870c51b11
Ayabonga Qwabi <aya@simply.co.za>
2024-07-05 17:48:35 +0200

    add mailer

--- src/pages/api/call-me-back/add.js
+++ src/pages/api/call-me-back/add.js
@@ -1,4 +1,22 @@
 import clientPromise from '@/util/mongo';
+const nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: `${process.env.NEXT_PUBLIC_EMAIL}`, //use env vars here
+    pass: `${process.env.NEXT_PUBLIC_APP_PASSWORD}`, // and here
+  },
+});
+
+var mailOptions = {
+  from: 'namoota.zar@gmail.com',
+  to: 'ayabongaqwabi@gmail.com',
+  subject: 'New Website Call Me back',
+};
 
 export default async function handler(req, res) {
   try {
@@ -18,6 +36,15 @@ export default async function handler(req, res) {
       res
         .status(200)
         .json({ message: 'Testimonial added successfully', data: result });
+
+      mailOptions.html = `<h1>Hi Siavista,</h1> <p>This is Namoota, you have received a new call me back request on your website</p> <p>Name: ${data.name}</p> <p>Phone: <a href='tel:${data.cellphoneNumber}'>${data.cellphoneNumber} </a></p>`;
+      transporter.sendMail(mailOptions, function (err, info) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
     } else {
       res.status(405).json({ message: 'Method not allowed' });
     }
